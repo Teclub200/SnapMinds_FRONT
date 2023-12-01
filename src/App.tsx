5f95f1d612d199a14a6ef76f0750a6fbe24c692f
KHashimoto3 <hashi.koki385@gmail.com>
2023-12-01 09:16:51 +0900

    Add react-router-dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./components/pages/HomePage";
+
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
