c73856c98dd8e704c5764b64a79abd024f3ef8cb
KHashimoto3 <hashi.koki385@gmail.com>
2023-12-02 14:55:17 +0900

    Add Typography

--- src/components/features/top/Top.tsx
+++ src/components/features/top/Top.tsx
@@ -1,7 +1,41 @@
+import { Box, Typography } from "@mui/material";
+
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+
 export const Top = () => {
   return (
     <div>
-      <h1>Top</h1>
+      <Box sx={{ flexGrow: 1, backgroundColor: "#999999" }}>
+        <Box
+          sx={{
+            width: "80%",
+            height: "100vh",
+            margin: "auto",
+            paddingTop: "10%",
+            textAlign: "center",
+          }}
+        >
+          <CameraAltIcon
+            sx={{
+              fontSize: 100,
+              color: "#FFFFFF",
+              marginTop: "30px",
+            }}
+          />
+          <Typography
+            variant="h3"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              color: "#FFFFFF",
+              fontWeight: "bold",
+              marginTop: "10%",
+            }}
+          >
+            写真のスキルを学ぶ・共有する
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 };
