533392a852313e4f829c1f498ca4e6e5444076ec
KHashimoto3 <hashi.koki385@gmail.com>
2023-12-03 10:50:20 +0900

    Fix recipe data type and add likecount

--- src/components/features/recipeDetail/RecipeDetail.tsx
+++ src/components/features/recipeDetail/RecipeDetail.tsx
@@ -1,12 +1,14 @@
 import { Box, Typography, Grid } from "@mui/material";
 
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Recipe } from "../../types/recipe";
 
 export const RecipeDetail = () => {
   const recipeData: Recipe = {
     id: 1,
     title: "ここにタイトルが入ります",
+    thumbnail: "https://picsum.photos/200",
     description: "ここに簡単な説明が入ります。",
     process: [
       "ここに手順1が入ります。ここに手順1が入ります。ここに手順1が入ります。",
@@ -15,8 +17,11 @@ export const RecipeDetail = () => {
       "ここに手順4が入ります。ここに手順3が入ります。ここに手順3が入ります。",
     ],
     advice: "ここにアドバイスが入ります。",
-    deviceUsed: "ここに撮影デバイス名が入ります。",
     genreId: 1,
+    likeCount: 99,
+    createdAt: "2021-10-01",
+    deviceUsed: "ここに撮影デバイス名が入ります。",
+    owner: 1,
   };
 
   const genreName: string = "春"; //仮で入れています。
@@ -44,10 +49,20 @@ export const RecipeDetail = () => {
           <Typography variant="h4" component="div" sx={{ fontWeight: "bold" }}>
             {recipeData.title}
           </Typography>
-          <Typography variant="h6" component="div">
-            <AccountCircleIcon fontSize="medium" />
-            {userName}
-          </Typography>
+          <Grid container spacing={2} sx={{ marginTop: "5px" }}>
+            <Grid item xs={9}>
+              <Typography variant="h6" component="div">
+                <AccountCircleIcon sx={{ verticalAlign: "-5px" }} />
+                {userName}
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6" component="div">
+                <FavoriteBorderIcon sx={{ verticalAlign: "-5px" }} />
+                {recipeData.likeCount}
+              </Typography>
+            </Grid>
+          </Grid>
           <Box
             sx={{
               flexGrow: 1,
@@ -120,7 +135,7 @@ export const RecipeDetail = () => {
             撮影日時
           </Typography>
           <Typography variant="h5" component={"div"} sx={{ marginTop: "5px" }}>
-            撮影日時が入ります。
+            {recipeData.createdAt}
           </Typography>
           <Typography
             variant="h4"
--- src/components/types/recipe.ts
+++ src/components/types/recipe.ts
@@ -1,9 +1,13 @@
 export type Recipe = {
   id: number;
   title: string;
+  thumbnail: string;
   description: string;
   process: string[];
   advice: string;
-  deviceUsed: string;
   genreId: number;
+  likeCount: number;
+  createdAt: string;
+  deviceUsed: string;
+  owner: number;
 };
